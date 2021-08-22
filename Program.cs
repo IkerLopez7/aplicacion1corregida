6121acd16daf97f2f183d86648b1ef05318dd875
Iker <85662488+IkerLopez7@users.noreply.github.com>
2021-08-21 23:45:46 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string archivo;
+            int p;
+            string R = "s";
+            while (R == "s")
+            {
+                Console.WriteLine("introduzca el numero que corresponda a la proceso deseado\n1. Crear \n2. Salir");
+                p = int.Parse(Console.ReadLine());
+
+
+                if (p == 1)
+                {
+                    StreamWriter ar;
+                    archivo = Console.ReadLine();
+                    Console.WriteLine("Escriba el nombre que desea ponerle al arhivo: ");
+                    ar = File.CreateText(Console.ReadLine() + ".txt");
+                    ar.Close();
+                }
+                Console.Clear();
+
+                if (p == 2)
+                {
+                    Console.WriteLine("Usted a seleccionado la opcion salir");
+                }
+                Console.Clear();
+                Console.WriteLine("Quieres crear otro archivo?");
+                Console.WriteLine("s/n");
+                R = Console.ReadLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
